5856a90a309be8bfe588272f9d08f692bd3a9951
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2026-05-15 00:08:42 +0300

    Implement ApplicationContext using ApplicationContext with XML configuration

--- src/main/java/com/abdullah/ecommerce/MainApp.java
+++ src/main/java/com/abdullah/ecommerce/MainApp.java
@@ -2,8 +2,11 @@ package com.abdullah.ecommerce;
 
 import com.abdullah.ecommerce.model.Product;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
 
 public class MainApp {
     public static void main(String[] args) {
@@ -11,11 +14,15 @@ public class MainApp {
 //        Product product = new Product();
 //        product.displayProduct();
 
-        BeanFactory factory =
-                new XmlBeanFactory(new ClassPathResource("beans.xml"));
-
+        // used BeanFactory
+//        BeanFactory factory =
+//                new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        
+        // used ApplicationContext
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("beans.xml");
         Product product =
-                (Product) factory.getBean("productBean");
+                (Product) context.getBean("productBean");
 
         product.displayProduct();
 
