13287144535010d9c7cb88acf06e00a7540931a2
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2026-05-14 17:02:35 +0300

    Initial commit: setup Spring Core ecommerce projec

---
+++ src/main/java/com/abdullah/ecommerce/MainApp.java
@@ -0,0 +1,12 @@
+package com.abdullah.ecommerce;
+
+import com.abdullah.ecommerce.model.Product;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        Product product = new Product();
+        product.displayProduct();
+
+    }
+}
---
+++ src/main/java/com/abdullah/ecommerce/model/Product.java
@@ -0,0 +1,7 @@
+package com.abdullah.ecommerce.model;
+
+public class Product {
+    public void displayProduct() {
+        System.out.println("Product Bean Created Successfully");
+    }
+}
