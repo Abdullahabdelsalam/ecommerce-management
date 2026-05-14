f81dcc4a50fc25c7f4be07c48c99c162ea547abe
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2026-05-14 23:59:10 +0300

    Implement BeanFactory using XmlBeanFactory with XML configuration

--- src/main/java/com/abdullah/ecommerce/MainApp.java
+++ src/main/java/com/abdullah/ecommerce/MainApp.java
@@ -1,11 +1,22 @@
 package com.abdullah.ecommerce;
 
 import com.abdullah.ecommerce.model.Product;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 
 public class MainApp {
     public static void main(String[] args) {
 
-        Product product = new Product();
+//        Product product = new Product();
+//        product.displayProduct();
+
+        BeanFactory factory =
+                new XmlBeanFactory(new ClassPathResource("beans.xml"));
+
+        Product product =
+                (Product) factory.getBean("productBean");
+
         product.displayProduct();
 
     }
